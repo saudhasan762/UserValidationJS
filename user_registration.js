b2881f9354df94e3e816a59c64cf04a75a48f92a
saudhasan762 <azanali152@gmail.com>
2021-04-20 00:07:29 +0530

    Added: validating email as per the given condition

--- user_registration.js
+++ user_registration.js
@@ -13,5 +13,12 @@ const userValidation = () => {
         console.log("Valid Last Name");
     else
         console.log("Invalid Last Name");
+
+    let email = prompt('Enter the email address : ');
+    let emailRegex =RegExp('^[a-zA-Z0-9]+([_+-.]{0,1})([@]{1})[a-z0-1]+.[a-z]+(.?)([a-z]{2}?)$');
+    if(emailRegex.test(email))
+        console.log("Valid Email");
+    else
+        console.log("Invalid Email");
 }
 userValidation();
